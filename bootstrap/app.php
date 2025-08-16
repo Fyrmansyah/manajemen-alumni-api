854684c2620ddebeade9e7a621a492c497144e84
Fyrmansyah <firmansyahrizki278@gmail.com>
2025-08-16 22:59:26 +0700

    feat: add global exception handler for api NotFoundHttpException

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,18 @@
 <?php
 
+use App\Helpers\ResponseBuilder;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withCommands([
@@ -27,5 +31,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return ResponseBuilder::fail()
+                    ->httpCode(Response::HTTP_NOT_FOUND)
+                    ->message('Data Tidak Ditemukan')
+                    ->build();
+            }
+        });
     })->create();
